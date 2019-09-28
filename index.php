feed69f1ef1d6924d91ef90180bf65c30e37a94c
herminio-git <git.herminio@gmail.com>
2019-09-28 19:56:31 -0300

    Tela de Login

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="css/login.css">
+  </head>
+  <body>
+
+        <div class="container">
+            <div class="card card-login mx-auto text-center bg-dark">
+                <div class="card-header mx-auto bg-dark">
+                    <span> <img src="img/logo-elefante.png" class="w-50" alt="Logo"> </span><br/>
+                                <span class="logo_title mt-5"> Login Dashboard </span>
+        <!--            <h1>--><?php //echo $message?><!--</h1>-->
+
+                </div>
+                <div class="card-body">
+                    <form action="login.php" method="post">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" name="usuario" class="form-control" placeholder="Usuário...">
+                        </div>
+
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" name="senha" class="form-control" placeholder="Senha...">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" name="btn" value="LOGIN" class="btn btn-outline-danger float-right login_btn">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+  </body>
+</html>
+
+
+
+
+
---
+++ painel_admin.php
@@ -0,0 +1,8 @@
+<?php 
+
+    session_start();
+    include('verificar_login.php');
+
+?>
+
+<a href="logout.php">Sair</a>
